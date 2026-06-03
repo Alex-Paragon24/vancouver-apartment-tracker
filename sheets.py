969e63af902a0996b757f4e26a9baaef91f4d069
Alex Abrosimov <abrosimov.digital@gmail.com>
2026-06-02 21:35:09 -0700

    Remove unused datetime import

--- sheets.py
+++ sheets.py
@@ -2,7 +2,6 @@ import os
 import re
 import logging
 import gspread
-from datetime import datetime, timezone
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
